e92820ffdfa7eb4d5af8cf8f8dace33b2857b5ef
baidikov-roman-mi <wolkdem@gmail.com>
2023-10-13 14:23:40 +0300

    UI done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import removeLogo from "../public/delete-icon.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="display">
+          <div className="solving">12 + 1 - 1 * 10</div>
+          <div className="output"> = 120</div>
+        </div>
+        <div className="calculations">
+          <div className="clearAll">Ac</div>
+          <div className="remove">
+            <img src={removeLogo} alt="remove" />
+          </div>
+          <div className="divide">/</div>
+          <div className="multiply">*</div>
+          <div className="number">1</div>
+          <div className="number">2</div>
+          <div className="number">3</div>
+          <div className="minus">-</div>
+          <div className="number">4</div>
+          <div className="number">5</div>
+          <div className="number">6</div>
+          <div className="plus">+</div>
+          <div className="number">7</div>
+          <div className="number">8</div>
+          <div className="number">9</div>
+          <div className="number">0</div>
+          <div className="number">00</div>
+          <div className="dot">.</div>
+          <div className="equals">=</div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
